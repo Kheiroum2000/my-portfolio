a9e34d4b1c11a3e4a5de304237e809d05dcf86bf
Kheiroum2000 <kheiroum.jehangir@gmail.com>
2026-06-18 18:25:59 +0500

    add css module, darkMode dynamic and .map()

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,32 @@
 import { useContext } from 'react'
 import ThemeContext from '../ThemeContext'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { data } from 'react-router-dom'
+import styles from './Projects.module.css'
 
 function Projects() {
+    const [projectData, setProjectData] = useState(null)
+    useEffect(()=>{
+        fetch('https://express-my-portfolio-production.up.railway.app/projects')
+        .then(res => res.json())
+        .then(data => setProjectData(data))
+    }, [])
     const { darkMode } = useContext(ThemeContext)
     return (
         <div style={{ padding: '60px 40px', backgroundColor: darkMode ? '#1a1a1a' : '#ffffff', minHeight: '100vh'}}>
 
             <h1 style={{ color: darkMode ? 'white' : '#111', fontSize: '48px'}}>My Projects</h1>
 
-            <p style={{color: darkMode ? '#aaa' : '#555' , marginTop: '20px', fontSize: '18px'}}>
-                Projects coming soon...
-            </p>
+            {projectData ? (
+                projectData.map((project)=>(
+                    <div className={`${styles.projectCard} ${darkMode ? styles.projectCardDark : styles.projectCardLight}`} key={project.id}>
+                        <h3 className={`${styles.projectTitle} ${darkMode ? styles.projectTitleDark : styles.projectTitleLight}`}>{project.project}</h3>
+                    </div>
+                ))
+            ) : (
+                <p>Loading projects...</p>
+            )}
         </div>
     )
 }
